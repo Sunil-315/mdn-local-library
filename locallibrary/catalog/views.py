a899da02b8b88085dfbab99cbf2c7cfcd3efa871
Sunil-315 <135532616+Sunil-315@users.noreply.github.com>
2025-11-30 10:34:03 +0530

    Added book list and book detail pages

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_instances_avail = BookInstance.objects.filter(status__exact='a').count()
+
+    num_authors = Author.objects.count()
+
+    num_genres = Genre.objects.filter(name__contains='Fiction').count()
+
+    books_count = Book.objects.filter(author__first_name__icontains='Tim').count()
+
+    context = {
+        'num_books' : num_books,
+        'num_instances' : num_instances,
+        'num_instances_available' : num_instances_avail,
+        'num_authors' : num_authors,
+        'num_genres' : num_genres,
+        'books_count' : books_count,
+    }
+
+    return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
